28f24c136e513b2653040d51f19bad0fcc72d6e3
ildar.serazitdinov@itbrick.ru <XeoARMrV%>
2019-08-19 17:11:55 +0300

    7 kyu kata

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -19,7 +19,7 @@ public class Kata {
     public static char findMissingLetter(char[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i + 1] != (array[i] + 1))
-                //return из тела цикла это менее читабельно и считается bad practise
+                //return из тела цикла это менее читабельно и считается bad practise, хотя для небольших методов сойдет
                 return (char)(array[i] + 1);
         }
         return ' ';
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 225;
-        System.out.println((i % 10));
+
     }
 }
